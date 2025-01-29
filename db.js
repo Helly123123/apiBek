cb98854819c427013cf9986c47d7e08a180f52a2
helly <maksim.birykov.2007@mail.ru>
2025-01-29 18:59:31 +0500

    fdfd

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 // db.js
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root", // Замените на ваше имя пользователя
-  password: "root", // Замените на ваш пароль
-  database: "payments", // Убедитесь, что указана база данных
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
+  connectionLimit: 10,
 });
 
 // Подключаемся к MySQL и создаем базу данных, если она не существует
